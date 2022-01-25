0efff0a8955e027d9368d94b0485d2f26720a97c
RedstoneWizard22 <redstone@rwizard22.com>
2022-01-25 21:51:51 +0000

    Create projection page
    now with cube!

--- src/routes/projection.svelte
+++ src/routes/projection.svelte
@@ -1,1 +1,154 @@
-<p>Projection</p>
+<script lang="ts">
+  import { onDestroy, onMount } from 'svelte';
+  import * as THREE from 'three';
+  import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+  import WireframeObject from '../scripts/wireframeobject';
+
+  import cell8Data from '../assets/wireframes/cell8.json';
+
+  let canvas: HTMLCanvasElement;
+  // let height: number, width: number;
+  let canvasParent: HTMLElement;
+  let animationFrame: number;
+
+  let scene: THREE.Scene,
+    camera: THREE.PerspectiveCamera,
+    renderer: THREE.WebGLRenderer;
+  let cube: WireframeObject;
+
+  onMount(() => {
+    init();
+    // Add resize event listener to the window
+    window.addEventListener('resize', resize, false);
+    startAnimating(90);
+  });
+
+  onDestroy(() => {
+    destroy();
+  });
+
+  function init() {
+    // Set canvas width and height
+    const sceneWidth = canvasParent.clientWidth;
+    const sceneHeight = canvasParent.clientHeight;
+
+    ////////// Inital Setup //////////
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+      75,
+      sceneWidth / sceneHeight,
+      0.1,
+      1000
+    );
+    renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+      antialias: true,
+      alpha: true,
+    });
+    renderer.setSize(sceneWidth, sceneHeight);
+    renderer.sortObjects = false;
+
+    // scene.background = new THREE.Color(0xf8f8ff);
+    // scene.background = new THREE.Color( 0xffdf06 );
+    scene.background = new THREE.Color(0xf9fafb);
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.shadowMap.enabled = true;
+    renderer.setSize(sceneWidth, sceneHeight);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    ////////// Scene Setup //////////
+    // Camera
+    camera.position.set(0, 2, -5);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    controls.update(); // OrbitControls must be updated after changes to camera position/rotation
+
+    // Objects
+    const floorGeometry = new THREE.PlaneGeometry(22, 22);
+    const floorMaterial = new THREE.ShadowMaterial({ opacity: 0.3 });
+    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.name = 'floor';
+    floor.position.y = -4;
+    floor.rotateX(-Math.PI / 2);
+    floor.receiveShadow = true;
+
+    scene.add(floor);
+
+    // Lighting
+    const ambientLight = new THREE.AmbientLight(0xc4c4c4, 0.7);
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.width = 2048;
+    directionalLight.shadow.mapSize.height = 2048;
+    directionalLight.position.y = 10;
+    scene.add(directionalLight);
+
+    cube = new WireframeObject(scene, cell8Data);
+    cube.updateMeshes();
+  }
+
+  let fpsInterval: number,
+    startTime: number,
+    now: number,
+    then: number,
+    elapsed: number;
+
+  // initialize the timer variables and start the animation
+
+  function startAnimating(fps: number) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    animate();
+  }
+
+  function animate() {
+    // request another frame
+    requestAnimationFrame(animate);
+
+    // calc elapsed time since last loop
+    now = Date.now();
+    elapsed = now - then;
+
+    // if enough time has elapsed, draw the next frame
+    if (elapsed > fpsInterval) {
+      // Get ready for next frame by setting then=now, but also adjust for your
+      // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+      then = now - (elapsed % fpsInterval);
+
+      // Put your drawing code here
+      cube.rotate({ xw: 0.01 });
+      cube.projectTo3D();
+      cube.updateMeshes();
+      renderer.render(scene, camera);
+    }
+  }
+
+  function resize() {
+    const sceneWidth = canvasParent.clientWidth;
+    const sceneHeight = canvasParent.clientHeight;
+    renderer.setSize(sceneWidth, sceneHeight);
+    camera.aspect = sceneWidth / sceneHeight;
+    camera.updateProjectionMatrix();
+  }
+
+  function destroy() {
+    cancelAnimationFrame(animationFrame);
+    renderer.dispose();
+
+    window.removeEventListener('resize', resize, false);
+  }
+</script>
+
+<div class="flex h-screen p-2 bg-gray-100">
+  <div class="w-8/12 h-full p-2">
+    <div class="w-full h-full shadow bg-gray-50" bind:this={canvasParent}>
+      <canvas width="100" height="100" bind:this={canvas} />
+    </div>
+  </div>
+  <div class="w-4/12 h-full p-2">
+    <div class="w-full h-full shadow bg-gray-50" />
+  </div>
+</div>
